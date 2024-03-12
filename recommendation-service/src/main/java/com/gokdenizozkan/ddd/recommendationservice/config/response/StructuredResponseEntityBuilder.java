3479c58e9c5432463162f220bfa63c5caffbe2f4
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-03-13 01:21:02 +0300

    add Custom response types (sharing same response structure across all services) - v0.9.2-f11[2].10

---
+++ recommendation-service/src/main/java/com/gokdenizozkan/ddd/recommendationservice/config/response/StructuredResponseEntityBuilder.java
@@ -0,0 +1,42 @@
+package com.gokdenizozkan.ddd.recommendationservice.config.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class StructuredResponseEntityBuilder <T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private HttpStatus httpStatus;
+
+    private StructuredResponseEntityBuilder() {
+    }
+
+    public static <T> StructuredResponseEntityBuilder<T> builder() {
+        return new StructuredResponseEntityBuilder<>();
+    }
+
+    public StructuredResponseEntityBuilder<T> success(boolean success) {
+        this.success = success;
+        return this;
+    }
+
+    public StructuredResponseEntityBuilder<T> message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public StructuredResponseEntityBuilder<T> data(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public StructuredResponseEntityBuilder<T> httpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+        return this;
+    }
+
+    public ResponseEntity<Structured<T>> build() {
+        return ResponseEntity.status(httpStatus).body(new Structured<T>(success, message, data));
+    }
+}
