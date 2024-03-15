d83cb57e2c7cdb8ccc486d16d08c3c9f53588669
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-03-16 02:40:58 +0300

    dockerised main-service successfuly

--- main-service/src/main/java/com/gokdenizozkan/ddd/mainservice/DddMainService.java
+++ main-service/src/main/java/com/gokdenizozkan/ddd/mainservice/DddMainService.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class DddGeneralServiceApplication {
+public class DddMainService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DddGeneralServiceApplication.class, args);
+		SpringApplication.run(DddMainService.class, args);
 	}
 
 }
